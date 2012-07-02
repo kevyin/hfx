66840cc4a3b3a2f46e0f6223e8fb6f4fdc8ee2ea
Kevin Ying <ying.kev@gmail.com>
2012-07-02 23:12:49 +1000

    version number, error: no output file in MainGen

--- src/haskell/MainGen.hs
+++ src/haskell/MainGen.hs
@@ -20,6 +20,7 @@ import Data.Maybe
 import Control.Monad
 import System.IO
 import System.Environment
+import System.Exit
 import qualified Data.Vector.Generic as G
 
 
@@ -28,6 +29,10 @@ main = do
   args   <- getArgs
   (cp,f) <- sequestConfig args
   let fp =  fromMaybe (error "Protein database not specified") (databasePath cp)
+  case outputPath cp of
+       Just path -> when (verbose cp) $ hPutStrLn stderr ("Output file: " ++ show path)
+       _         -> do hPutStrLn stderr $ "ERROR: No output path was specified\n" 
+                       exitFailure
 
   (t,dbs') <- bracketTime $ makeSeqDB cp fp (splitDB cp)
   when (verbose cp) $ hPutStrLn stderr ("Reading time: " ++ showTime t)
